50d864c12ce84d837ccdc1137195294ad898216c
lucas890 <78564060+llucas890@users.noreply.github.com>
2021-08-19 23:18:49 -0300

    Add files via upload

--- piedra papel o tijera/piedra papel o tijera.py
+++ piedra papel o tijera/piedra papel o tijera.py
@@ -1,46 +1,60 @@
 import random
 import msvcrt
 
+
 puntaje = 0
-eleccion = (input('Piedra, Papel o Tijera: '))
+eleccion = None
 lista = ('Piedra', 'Papel', 'Tijera')
-robot = random.choice(lista)
-print(f'El robot elijio {robot}')
-if eleccion == 'Piedra':
-    if robot == 'Piedra':
-        print('Los dos eligieron piedra es un empate')
-    elif robot == 'Papel':
-        puntaje -= 1
-        print('Perdiste gana papel')
-    elif robot == 'Tijera':
-        puntaje += 1
-        print('Gana piedra')
-    else:
-        print('Debes elegir uno de los 3')
-elif eleccion == 'Papel':
-    if robot == 'Piedra':
-        puntaje -= 1
-        print('Perdiste gana piedra')
-    elif robot == 'Papel':
-        puntaje += 1
-        print('Gana tijera')
-    elif robot == 'Tijera':
-        print('Empate')
-    else:
-        print('Debes elegir uno de los 3')
-elif eleccion == 'Tijera':
-    if robot == 'Piedra':
-        puntaje += 1
-        print('Gana papel')
-    elif robot == 'Papel':
-        print('Empate')
-    elif robot == 'Tijera':
-        puntaje -= 1
-        print('Perdiste gana tijera')
-    else:
-        print('Debes elegir uno de los 3')
-else:
-    print('Debes ingresar un valor correcto')
+print('Bienvenido a Piedra, papel o tijera. Diviertete')
+print('Si deseas salir, escribe: Salir')
+print('Si deseas ver tu puntaje, escribe: Puntaje')
+while eleccion != 'Salir':
+    print('Piedra, Papel o Tijera' + Fore.GREEN + Fore.RESET)
+    eleccion = input('Cual es tu eleccion: \n')
+    if eleccion == 'Puntaje':
+        print(f'Tu puntaje es: {puntaje}')
+    if eleccion == 'Ayuda':
+        print('Nadie puede ayudarte ahora')
+    if eleccion == 'Piedra':
+        robot = random.choice(lista)
+        print(f'El robot elijio {robot}')
+        if robot == 'Piedra':
+            print('Los dos eligieron piedra... es un empate! \n')
+        elif robot == 'Papel':
+            puntaje -= 1
+            print('Perdiste!!')
+            print(f'Tu puntaje es: {puntaje} \n')
+        elif robot == 'Tijera':
+            puntaje += 1
+            print('Ganaste!!')
+            print(f'Tu puntaje es: {puntaje} \n')
+    elif eleccion == 'Papel':
+        robot = random.choice(lista)
+        print(f'El robot elijio {robot}')
+        if robot == 'Piedra':
+            puntaje -= 1
+            print('Perdiste!!')
+            print(f'Tu puntaje es: {puntaje}\n')
+        elif robot == 'Papel':
+            puntaje += 1
+            print('Ganaste!!')
+            print(f'Tu puntaje es: {puntaje}\n')
+        elif robot == 'Tijera':
+            print('Los dos eligieron tijera... es un empate! \n')
+    elif eleccion == 'Tijera':
+        robot = random.choice(lista)
+        print(f'El robot elijio {robot}')
+        if robot == 'Piedra':
+            puntaje += 1
+            print('Ganaste!!')
+            print(f'Tu puntaje es: {puntaje}\n')
+        elif robot == 'Papel':
+            print('Los dos eligieron papel... es un empate\n')
+        elif robot == 'Tijera':
+            puntaje -= 1
+            print('Perdiste!!!')
+            print(f'Tu puntaje es: {puntaje}\n')
+print('Espero te hayas divertido!')
 print('Programa creado por Lucas Lescano')
 print('Presiona una tecla para salir...')
 msvcrt.getch()
