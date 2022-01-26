50d074334a178d0acbd4f1dc02fa01fab4b7b200
lucas890 <78564060+llucas890@users.noreply.github.com>
2022-01-25 23:55:41 -0300

    Add files via upload

--- pasalista.py
+++ pasalista.py
@@ -17,30 +17,39 @@ while busqueda != '6':
     print('6 Para salir solamente sin guardar')
     print('7 Para restar 20 asist')
     print('8 Para restar 3 asist semanales')
+    print('9 Eliminar asistencias pasada')
     print('Programa creado por Lucas Lescano alias Desxz')
     print('')
     busqueda = input('Ingresa la opcion: \n')
     print('')
     if busqueda == '1':
         while busqueda != 'SALIR':
-                busqueda = input('Introduce la lista: ')         
+                busqueda = input('////// Introduce la lista: /////')         
                 busqueda = busqueda.upper()
+                busqueda = busqueda.strip()
+                busqueda = busqueda.replace(" ", "")
+                busqueda = busqueda.replace("_", "")
+                busqueda = busqueda.replace("-", "")
+                busqueda = busqueda.replace("0", "o")
+                busqueda = busqueda.replace("SrMeLiОDaS", "MELIO")
+                busqueda = busqueda.replace(".", "")
+                busqueda = busqueda.replace("sm", "")
                 for colA,colNB,colC, colND,colE, colNF,colG, colNH, colI, colNJ in wb:
                     if busqueda in colA.value:
                         wb[f'B{colNB.row}'] = colNB.value + 1
-                        print(f"Nombre encontrado: {busqueda}. Asistencias: {wb[f'B{colNB.row}'].value}\n")
+                        print(f"\nNombre encontrado: {busqueda}. Asistencias: {wb[f'B{colNB.row}'].value}\n")
                     elif busqueda in colC.value:
                         wb[f'D{colND.row}'] = colND.value + 1
-                        print(f"Nombre encontrado: {busqueda}. Asistencias: {wb[f'D{colND.row}'].value}\n")
+                        print(f"\nNombre encontrado: {busqueda}. Asistencias: {wb[f'D{colND.row}'].value}\n")
                     elif busqueda in colE.value:
                         wb[f'F{colNF.row}'] = colNF.value + 1
-                        print(f"Nombre encontrado: {busqueda}. Asistencias: {wb[f'F{colNF.row}'].value}\n")
+                        print(f"\nNombre encontrado: {busqueda}. Asistencias: {wb[f'F{colNF.row}'].value}\n")
                     elif busqueda in colG.value:
                         wb[f'H{colNH.row}'] = colNH.value + 1
-                        print(f"Nombre encontrado: {busqueda}. Asistencias: {wb[f'H{colNH.row}'].value}\n")
+                        print(f"\nNombre encontrado: {busqueda}. Asistencias: {wb[f'H{colNH.row}'].value}\n")
                     elif busqueda in colI.value:
                         wb[f'J{colNJ.row}'] = colNJ.value + 1
-                        print(f"Nombre encontrado: {busqueda}. Asistencias: {wb[f'J{colNJ.row}'].value}\n")
+                        print(f"\nNombre encontrado: {busqueda}. Asistencias: {wb[f'J{colNJ.row}'].value}\n")
     if busqueda == '2':
         busqueda = input('Ingrese el nombre para restar 34 asist: ')
         busqueda = busqueda.upper()
@@ -104,27 +113,53 @@ while busqueda != '6':
             cell = wb.cell(row=row, column=2)
             if cell.value is not wb['B17']:
                 cell.value = cell.value - 3
-                print('Restando en la columna 2')
+            print('Restando en la columna 2')
         for row in range(2, wb.max_row+1):
             cell = wb.cell(row=row, column=4)
             if cell.value is not wb['B17']:
                 cell.value = cell.value - 3
-                print('Restando en la columna 4')
+            print('Restando en la columna 4')
         for row in range(2, wb.max_row+1):
             cell = wb.cell(row=row, column=6)
             if cell.value is not wb['B17']:
                 cell.value = cell.value - 3
-                print('Restando en la columna 6')
+            print('Restando en la columna 6')
         for row in range(2, wb.max_row+1):
             cell = wb.cell(row=row, column=8)
             if cell.value is not wb['B17']:
                 cell.value = cell.value - 3
-                print('Restando en la columna 8')
+            print('Restando en la columna 8')
         for row in range(2, wb.max_row+1):
             cell = wb.cell(row=row, column=10)
             if cell.value is not wb['B17']:
                 cell.value = cell.value - 3                
-                print('Restando en la columna 10')
+            print('Restando en la columna 10')
         stock = wb['B17']
         stock.value = stock.value + 3
         print(stock.value)
+    if busqueda == '9':
+        while busqueda != 'SALIR':
+                busqueda = input('////// Introduce A RESTAR: /////')         
+                busqueda = busqueda.upper()
+                busqueda = busqueda.strip()
+                busqueda = busqueda.replace(" ", "")
+                busqueda = busqueda.replace("-", "")
+                busqueda = busqueda.replace("0", "o")
+                busqueda = busqueda.replace("SrMeLiОDaS", "MELIO")
+                busqueda = busqueda.replace(".", "")
+                for colA,colNB,colC, colND,colE, colNF,colG, colNH, colI, colNJ in wb:
+                    if busqueda in colA.value:
+                        wb[f'B{colNB.row}'] = colNB.value - 1
+                        print(f"\nNombre encontrado: {busqueda}. Asistencias: {wb[f'B{colNB.row}'].value}\n")
+                    elif busqueda in colC.value:
+                        wb[f'D{colND.row}'] = colND.value - 1
+                        print(f"\nNombre encontrado: {busqueda}. Asistencias: {wb[f'D{colND.row}'].value}\n")
+                    elif busqueda in colE.value:
+                        wb[f'F{colNF.row}'] = colNF.value - 1
+                        print(f"\nNombre encontrado: {busqueda}. Asistencias: {wb[f'F{colNF.row}'].value}\n")
+                    elif busqueda in colG.value:
+                        wb[f'H{colNH.row}'] = colNH.value - 1
+                        print(f"\nNombre encontrado: {busqueda}. Asistencias: {wb[f'H{colNH.row}'].value}\n")
+                    elif busqueda in colI.value:
+                        wb[f'J{colNJ.row}'] = colNJ.value - 1
+                        print(f"\nNombre encontrado: {busqueda}. Asistencias: {wb[f'J{colNJ.row}'].value}\n")
